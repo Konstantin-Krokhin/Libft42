846dafea10a64f7bb6e03c7dac90d15c9c8c0e7b
Konstantin Krokhin <kokrokhi@students.42wolfsburg.de>
2021-12-23 13:12:38 +0100

    itoa fully written and adhered to norm

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,10 +12,79 @@
 
 #include "libft.h"
 
+static char	*return_null_rtn(char **rtn)
+{
+	*rtn = malloc(sizeof(char) * 2);
+	**rtn = '0';
+	(*rtn)++;
+	**rtn = '\0';
+	return (*rtn - 1);
+}
+
+static char	*fill_in_rtn(char *revnum, int size, int sign, char *rtn)
+{
+	int	num;
+
+	num = 0;
+	if (sign == -1)
+		num++;
+	revnum -= size - 1;
+	sign = size - 2;
+	while (num <= size - 1 && sign >= 0)
+		rtn[num++] = revnum[sign--];
+	rtn[num] = '\0';
+	return (rtn);
+}
+
+static char	*loop(long nm, int sign, int size, long num)
+{
+	char	*rtn;
+	char	*revnum;
+
+	rtn = 0;
+	nm *= sign;
+	num = nm;
+	while (num >= 1)
+	{
+		num /= 10;
+		size++;
+	}
+	rtn = malloc(sizeof(char) * (size + 1));
+	if (sign == -1)
+		rtn[0] = '-';
+	else
+		size++;
+	revnum = malloc(sizeof(char) * (size));
+	revnum[size - 1] = '\0';
+	while (nm >= 1)
+	{
+		*revnum++ = nm % 10 + '0';
+		nm /= 10;
+	}
+	rtn = fill_in_rtn(revnum, size, sign, rtn);
+	return (rtn);
+}
+
 char	*ft_itoa(int n)
 {
-	void	*rtn;
+	char	*rtn;
+	int		sign;
+	long	num;
+	int		size;
+	long	nm;
 
-	rtn = malloc(1);
+	num = 0;
+	nm = n;
+	rtn = 0;
+	sign = 1;
+	size = 0;
+	if (nm < 0)
+	{
+		sign = -1;
+		size++;
+	}
+	else if (nm == 0)
+		return (return_null_rtn(&rtn));
+	rtn = loop(nm, sign, size, num);
 	return (rtn);
 }
